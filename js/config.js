84a7895ce2683ad21a3790cdb2e9c9402eedba55
Lai Duc Binh <laiducbinh@Lais-Mac-mini.local>
2023-03-25 12:47:35 +0700

    sd

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'ap-northeast-1_deCEfh8p3', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'ap-northeast-1_deCEfh8p3', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '4l14r2n0fr8cdn8lnescr3s4np', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-northeast-1' // e.g. us-east-2
     },
     api: {
